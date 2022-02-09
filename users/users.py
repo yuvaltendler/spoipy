74f8dc48b7ddd5c6a22dddfc86e5d225e7e54d6a
Yuval t <yuval.tendler@gmail.com>
2022-02-09 14:41:40 +0200

    implement PremiumUser

--- users/users.py
+++ users/users.py
@@ -2,8 +2,14 @@ from singleton import Singleton
 
 
 class FreeUser():
-    def __init__(self, playlists = {}):
+    def __init__(self, playlists={}):
         self.playlists = playlists
+
+
+class PremiumUser(FreeUser):
+    def __init__(self, playlists={}):
+        super().__init__(playlists)
+
 
 class UserManager(metaclass=Singleton):
     def __init__(self):
