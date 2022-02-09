3ed80716dbaf3f9bb091f1be9349799df699b29e
Yuval t <yuval.tendler@gmail.com>
2022-02-09 10:19:34 +0200

    implement Artist

--- music/music.py
+++ music/music.py
@@ -1,3 +1,9 @@
-class ArtistManager():
-    def __init__(self, artists = {}):
+class Artist:
+    def __init__(self, name: str, album = []):
+        self.name = name
+        self.album = album
+
+
+class ArtistManager:
+    def __init__(self, artists: {str: [Artist]} = {}):
         self.artists = artists
