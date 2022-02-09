3708f8395173814fe035ab8eb5fc321115514db6
Yuval t <yuval.tendler@gmail.com>
2022-02-09 15:24:44 +0200

    implement add playlist func

---
+++ tests/test_users.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from users.users import FreeUser
+
+
+class TestFreeUser(TestCase):
+    def test_add_playlist(self):
+        user = FreeUser()
+        user.playlists = {'p1': 'playlist', 'p2': 'playlist', 'p3': 'playlist', 'p4': 'playlist'}
+        user.add_playlist('p3', 'playlist')
+        assert user.playlists == {'p1': 'playlist', 'p2': 'playlist', 'p3': 'playlist', 'p4': 'playlist'}
+        user.add_playlist('p5', 'playlist')
+        assert user.playlists == {'p1': 'playlist', 'p2': 'playlist', 'p3': 'playlist', 'p4': 'playlist', 'p5': 'playlist'}
+        user.add_playlist('p6', 'playlist')
+        assert user.playlists == {'p1': 'playlist', 'p2': 'playlist', 'p3': 'playlist', 'p4': 'playlist', 'p5': 'playlist'}
+
--- users/users.py
+++ users/users.py
@@ -1,19 +1,43 @@
+import logging
+
+from music import Artist
 from singleton import Singleton
 
 
 class FreeUser():
-    def __init__(self, playlists={}):
-        self.playlists = playlists
+    MAX_PLAYLISTS = 5
+
+    def __init__(self):
+        self.playlists = {}
+
+    def add_playlist(self, name: str, playlist):
+        if len(self.playlists) >= FreeUser.MAX_PLAYLISTS:
+            logging.warning('User try to add more then %d playlist', FreeUser.MAX_PLAYLISTS)
+            # TODO raise error
+        elif name in self.playlists:
+            logging.warning('User try to add 2 playlists with the same name')
+            # TODO raise error
+        else:
+            self.playlists[name] = playlist
 
 
 class PremiumUser(FreeUser):
-    def __init__(self, playlists={}):
-        super().__init__(playlists)
+    def __init__(self):
+        super().__init__()
+
+    def add_playlist(self, name: str, playlist):
+        if name in self.playlists:
+            logging.warning('User try to add 2 playlists with the same name')
+            # TODO raise error
+        else:
+            self.playlists[name] = playlist
 
 
 class ArtistUser(PremiumUser):
-    def __init__(self, playlists={}):
-        super().__init__(playlists)
+    def __init__(self, artist: Artist):
+        super().__init__()
+        self.artist = artist
+
 
 
 class UserManager(metaclass=Singleton):
