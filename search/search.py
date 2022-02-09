d01310242fa4c6a4c9f7aecf34e038e028064262
Yuval t <yuval.tendler@gmail.com>
2022-02-09 18:05:22 +0200

    put decorator in note

--- search/search.py
+++ search/search.py
@@ -1,16 +1,17 @@
 from music import Artist, ArtistManager, Album, Song
 
+# def limit_search(func):
+#     def get_do_limit(do_limit: bool = False):
+#         def wrapper(*args, **kwargs):
+#             res = func(*args, **kwargs)
+#             if do_limit:
+#                 return res[:Search.MUX_RESULTS]
+#             return res
+#         return wrapper
+#     return get_do_limit
 
 class Search:
     MUX_RESULTS = 5
-    @staticmethod
-    def limit_search(func):
-        def limit_search(do_limit: bool = False, *args, **kwargs):
-            res = func(*args, **kwargs)
-            if do_limit:
-                return res[:Search.MUX_RESULTS]
-            return res
-        return limit_search
 
     # @limit_search
     @staticmethod
