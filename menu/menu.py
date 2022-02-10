53eebff53d3fe840d8fe7b9cf44611c8f600301b
Yuval t <yuval.tendler@gmail.com>
2022-02-10 09:47:50 +0200

    create subMenu search

--- menu/menu.py
+++ menu/menu.py
@@ -9,25 +9,33 @@ class Menu(metaclass=Singleton):
     def __init__(self):
         self.menu = ConsoleMenu('Spotipy', 'Welcome to spotipy')
         self.is_free_user = True
-        get_artists = FunctionItem('Get all artists', Menu._get_artists, [self])
-        get_albums = FunctionItem('Get artist\'s albums', Menu._get_albums, [self])
-        get_songs_in_album = FunctionItem('Get songs in album', Menu._get_songs_in_album, [self])
-        get_beast_songs = FunctionItem('Get beast songs', Menu._get_beast_songs, [self])
+
+        search_menu = self._get_search_menu()
+        search = SubmenuItem('Search', search_menu, self.menu)
 
         menu_item = MenuItem("Menu Item")
         function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
         command_item = CommandItem("Run a console command", "")  # "touch hello.txt"
         selection_menu = SelectionMenu(["item1", "item2", "item3"])
         submenu_item = SubmenuItem("Submenu item", selection_menu, self.menu)
-        self.menu.append_item(get_artists)
-        self.menu.append_item(get_albums)
-        self.menu.append_item(get_songs_in_album)
-        self.menu.append_item(get_beast_songs)
 
+        self.menu.append_item(search)
         self.menu.append_item(menu_item)
         self.menu.append_item(function_item)
         self.menu.append_item(command_item)
         self.menu.append_item(submenu_item)
+
+    def _get_search_menu(self):
+        search_menu = ConsoleMenu('Spotipy', 'Search menu')
+        get_artists = FunctionItem('Get all artists', Menu._get_artists, [self])
+        get_albums = FunctionItem('Get artist\'s albums', Menu._get_albums, [self])
+        get_songs_in_album = FunctionItem('Get songs in album', Menu._get_songs_in_album, [self])
+        get_beast_songs = FunctionItem('Get beast songs', Menu._get_beast_songs, [self])
+        search_menu.append_item(get_artists)
+        search_menu.append_item(get_albums)
+        search_menu.append_item(get_songs_in_album)
+        search_menu.append_item(get_beast_songs)
+        return search_menu
 
     def _get_artists(self):
         if self.is_free_user:
