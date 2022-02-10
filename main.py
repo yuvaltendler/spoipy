5a0c31bf62c5ce8aa69bc9a27fa2d228b2232413
Yuval t <yuval.tendler@gmail.com>
2022-02-10 16:15:39 +0200

    add users file path to properties.jsons

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from users import UserManager, PremiumUser
 
 
 def main():
-    if os.path.exists("log\\spotipy.log"):
-        os.remove("log\\spotipy.log")
-    logging.basicConfig(filename='log\\spotipy.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s',
-                        level=logging.DEBUG)
+    if os.path.exists(Properties().properties.get('LOG_FILE_PATH')):
+        os.remove(Properties().properties.get('LOG_FILE_PATH'))
+    logging.basicConfig(filename=Properties().properties.get('LOG_FILE_PATH'), filemode='a',
+                        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
     logging.debug('start main')
     load.load_music(Properties().properties.get('TRACKS_DIRECTORY_PATH'))
-    load.load_users('users_files\\users.json')
+    load.load_users(Properties().properties.get('USERS_PATH'))
     menu.show()
     logging.debug('stop main')
 
